abff8b05d69cf21727db0eac5cbcd87c44221391
BalamuruganCfdev2023 <balalearn9831@gmail.com>
2026-07-02 11:45:04 +0530

    Cleanup: remove duplicated root-level frontend files, keep client/ as single frontend source

--- src/utils/format.ts
+++
@@ -1,6 +0,0 @@
-export function formatLabel(value: string): string {
-  return value
-    .replace(/([a-z])([A-Z])/g, '$1 $2')
-    .replace(/_/g, ' ')
-    .replace(/\b\w/g, (char) => char.toUpperCase());
-}
